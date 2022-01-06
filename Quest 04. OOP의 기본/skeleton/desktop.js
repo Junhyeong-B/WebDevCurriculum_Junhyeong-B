d3aff4eb0f2882874c6480ae00549a4f35fb4985
JunHyeongB <aowlr0513@gmail.com>
2022-01-06 21:47:17 +0900

    feat: Quest 04. 폴더, 아이콘 드래그, 드랍 추가

--- Quest 04. OOP의 기본/skeleton/desktop.js
+++ Quest 04. OOP의 기본/skeleton/desktop.js
@@ -1,74 +1,147 @@
-class Desktop {
-  /* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-  constructor(icon, folder) {
-    this.icon = icon;
-    this.folder = folder;
+class Component {
+  $target;
+  $state;
+  constructor($target) {
+    this.$target = $target;
+    this.setup();
+    this.render();
+  }
 
-    const $desktop = document.querySelector(".desktop");
+  setup() {}
+  template() {
+    return "";
+  }
+  render() {
+    this.$target.innerHTML = this.template();
+    this.setEvent();
+  }
+  setEvent() {}
+  setState(newState) {
+    this.$state = { ...this.$state, newState };
+    this.render();
+  }
+}
 
-    for (let i = 0; i < folder; i++) {
-      const folder = document.createElement("div");
-      folder.className = "folder";
-      folder.setAttribute("data-id", i);
-      folder.innerText = `폴더 ${i}`;
-      $desktop.appendChild(folder);
-    }
+// folder = { type: 'folder', inner: [{}], title: string, id: number }
+// icon = { type: 'icon', title: string, id: number }
 
-    for (let i = 0; i < icon; i++) {
-      const icon = document.createElement("div");
-      icon.className = "icon";
-      icon.setAttribute("data-id", +i + +folder);
-      icon.innerText = `아이콘 ${+i + +folder}`;
-      $desktop.appendChild(icon);
-    }
+class Desktop extends Component {
+  $dragging;
+  setup() {
+    this.$state = {
+      states: [
+        { type: "folder", inner: [], title: "폴더1", id: 1 },
+        { type: "folder", inner: [], title: "폴더2", id: 2 },
+        { type: "folder", inner: [], title: "폴더3", id: 3 },
+        { type: "icon", title: "아이콘1", id: 4 },
+        { type: "icon", title: "아이콘2", id: 5 },
+        { type: "icon", title: "아이콘3", id: 6 },
+      ],
+    };
+  }
 
-    $desktop.addEventListener("click", (e) => {
-      if (e.target.className === "folder" || e.target.className === "icon") {
-        new Window(e.target);
+  template() {
+    const { states } = this.$state;
+    return `${states
+      .map((state) => {
+        const { type } = state;
+        if (type === "folder") {
+          return new Folder(this.$target, state).create();
+        } else {
+          return new Icon(this.$target, state).create();
+        }
+      })
+      .join("")}`;
+  }
+
+  setEvent() {
+    this.$target.addEventListener("dragstart", (e) => {
+      this.$dragging = e.target;
+      e.target.classList.add("dragging");
+    });
+
+    this.$target.addEventListener("dragend", (e) => {
+      this.$dragging = null;
+      e.target.classList.remove("dragging");
+    });
+
+    this.$target.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      if (
+        e.target.classList.contains("folder") ||
+        e.target.classList.contains("icon")
+      ) {
+        const afterElement = getDragAfterElement(this.$target, e.clientX);
+        if (!afterElement) {
+          this.$target.appendChild(this.$dragging);
+        } else {
+          this.$target.insertBefore(this.$dragging, afterElement);
+        }
       }
     });
+
+    function getDragAfterElement(target, x) {
+      const draggableElements = [
+        ...target.querySelectorAll("div:not(.dragging)"),
+      ];
+
+      return draggableElements.reduce(
+        (closest, child) => {
+          const box = child.getBoundingClientRect();
+          const offset = x - box.left - box.width / 2;
+          if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child };
+          } else {
+            return closest;
+          }
+        },
+        {
+          offset: Number.NEGATIVE_INFINITY,
+        }
+      ).element;
+    }
   }
 }
 
 class Icon {
-  /* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+  $target;
+  $state;
+  constructor($target, icon) {
+    const { title, id } = icon;
+    this.$target = $target;
+    this.$state = {
+      type: "icon",
+      title,
+      id,
+    };
+  }
+
+  create() {
+    return `
+      <div class="icon" id="${this.$state.id}" draggable="true">${this.$state.title}</div>
+    `;
+  }
 }
 
 class Folder {
-  /* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-}
-
-class Window {
-  /* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-  constructor(node) {
-    this.node = node;
-
-    node.onmousedown = function (event) {
-      node.classList.add("drag");
-      // node.style.position = "absolute";
-      // node.style.zIndex = 1000;
-
-      function moveAt(pageX, pageY) {
-        node.style.left = pageX - node.offsetWidth / 2 + "px";
-        node.style.top = pageY - node.offsetHeight / 2 + "px";
-      }
-
-      moveAt(event.pageX, event.pageY);
-
-      function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
-      }
-
-      document.addEventListener("mousemove", onMouseMove);
-
-      node.onmouseup = function () {
-        document.removeEventListener("mousemove", onMouseMove);
-        node.onmouseup = null;
-      };
-
-      node.ondragstart = function () {
-        return false;
-      };
+  $target;
+  $state;
+  constructor($target, icon) {
+    const { inner, title, id } = icon;
+    this.$target = $target;
+    this.$state = {
+      type: "folder",
+      inner,
+      title,
+      id,
     };
   }
+
+  create() {
+    return `
+      <div class="folder" id="${this.$state.id}" draggable="true">${this.$state.title}</div>
+    `;
+  }
 }
+
+class Window {}
